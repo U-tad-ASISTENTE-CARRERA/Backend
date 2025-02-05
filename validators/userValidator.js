dd7f8950381a95ba5044dfcc5aabb232efce3dc1
SashVqz <alvaro.vazquez.1716@gmail.com>
2025-02-05 17:32:47 +0100

    validators check

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,29 +1,80 @@
 const { body, param, query } = require("express-validator");
 
 const validateUser = [
-  body("name").isString().isLength({ min: 2 }).withMessage("Name must be at least 2 characters long"),
-  body("email")
-  .matches(/^[a-zA-Z0-9._%+-]+@(live\.u-tad\.com|u-tad\.com)$/)
-  .withMessage("Invalid email format, must be @live.u-tad.com or @u-tad.com"),
-  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
-  body("role").isIn(["STUDENT", "TEACHER", "ADMIN"]).withMessage("Invalid role"),
-  body("metadata").optional().isObject().withMessage("Metadata must be an object"),
+    body("name")
+    .isString().isLength({ min: 3 }).withMessage("Como minimo 3 caractéres de tamaño")
+    .isEmpty({ ignore_whitespace: false }).withMessage("Name cannot be empty"),
+    body("email")
+    .isString().isLength({ min: 3 }).withMessage("Como minimo 3 caractéres de tamaño")
+    .isEmpty({ ignore_whitespace: false })
+    .matches(/^[a-zA-Z0-9._%+-]+@(live\.u-tad\.com|u-tad\.com)$/)
+    .isEmpty({ ignore_whitespace: false }).withMessage("Email cannot be empty")
+    .withMessage("Invalid email format, must be @live.u-tad.com or @u-tad.com"),
+    body("password")
+    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters long")
+    .matches(/^(?=.*[A-Z])(?=.*[@$!%*?&]).$/),
+    body("metadata").optional().isObject().withMessage("Metadata must be an object"),
+    
+    //middleware para manejar errores
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
 ];
 
-const validateLogin = [
-  body("email").isEmail().withMessage("Invalid email format"),
-  body("password").isLength({ min: 6 }).withMessage("Password must be at least 6 characters long"),
+const valitdateLogin = [
+    body("email")
+    .isString().isLength({ min: 3 }).withMessage("Como minimo 3 caractéres de tamaño")
+    .isEmpty({ ignore_whitespace: false })
+    .matches(/^[a-zA-Z0-9._%+-]+@(live\.u-tad\.com|u-tad\.com)$/)
+    .isEmpty({ ignore_whitespace: false }).withMessage("Email cannot be empty")
+    .withMessage("Invalid email format, must be @live.u-tad.com or @u-tad.com"),
+    body("password")
+    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters long")
+    .matches(/^(?=.*[A-Z])(?=.*[@$!%*?&]).$/),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
 ];
 
 const validateUserId = [
-  param("id").isString().withMessage("Invalid user ID"),
+    param("id")
+    .isString().withMessage("Id cant be a string")
+    .isEmpty({ ignore_whitespace: false }).withMessage("Id cannot be empty"),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+
 ];
 
 const validateAcademicHistoryUpdate = [
-  param("id").isString().withMessage("Invalid user ID"),
-  body("subject").isString().withMessage("Subject must be a string"),
-  body("grade").isFloat({ min: 0, max: 10 }).withMessage("Grade must be between 0 and 10"),
-];
+    //Recibo de un pdf
+    param("id")
+    .isString().withMessage("Id cant be a string")
+    .isEmpty({ ignore_whitespace: false }).withMessage("Id cannot be empty"),
+    body("subject")
+    .isString().withMessage("Subject must be a string")
+    .isLength({ min: 3 }).withMessage("Subject must be at least 3 characters long"),
+    body("grade").isFloat({ min: 0, max: 10 }).withMessage("Grade must be between 0 and 10"),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    }
+]; 
 
 const validateRoleUpdate = [
   param("id").isString().withMessage("Invalid user ID"),
