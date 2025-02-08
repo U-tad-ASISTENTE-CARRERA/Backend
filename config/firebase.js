fd087c4e54c06fd93a227e8aa17939713dfe3649
SashVqz <alvaro.vazquez.1716@gmail.com>
2025-02-08 23:37:06 +0100

    railway testin

--- config/firebase.js
+++ config/firebase.js
@@ -2,7 +2,9 @@ const admin = require("firebase-admin");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const serviceAccount = require("../firebase.json");
+// const serviceAccount = require("../firebase.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS_JSON);
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
